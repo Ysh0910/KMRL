196fcb1c174010cd7af3983ec82b13f5cfc44afd
Gokul0Krishna <gokul.krishn1.2005@gmail.com>
2025-09-15 10:53:01 +0530

    working model

--- optimization_algorithm.py
+++ optimization_algorithm.py
@@ -215,3 +215,4 @@ if __name__ == "__main__":
     # print(c,d)
     print('SERVICE',best_plan.count('SERVICE'),'STANDBY',best_plan.count('STANDBY'),'MAINTENANCE',best_plan.count('MAINTENANCE'))
     print(score)
+    'hhi'
