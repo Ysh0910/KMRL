626975fb2aff432269797cb9fba95be905fb966d
GokulMariyappan <gokulmariyappan1@gmail.com>
2025-09-16 14:21:55 +0530

    made some changes to the model

--- backend/pipeline/data_pipeline.py
+++ backend/pipeline/data_pipeline.py
@@ -117,19 +117,19 @@ def process_data(all_data):
     # 1. Process Fitness Certificates
     fitness_data = all_data.get('fitness_certificates', [])
     for cert in fitness_data:
-        status = True
+        status = 'True'
         try:
             # Check validity date
             validity_date = datetime.strptime(cert['expiry_date'], '%d-%m-%Y').date()
             if validity_date < today:
-                status = False
+                status = 'False'
             
             # Check boolean fields if validity is ok
             if status:
                 if not (cert['braking'].lower() == 'true' and \
                         cert['signal'].lower() == 'true' and \
                         cert['structural_integrity'].lower() == 'true'):
-                    status = False
+                    status = 'False'
         except (ValueError, KeyError) as e:
             print(f"Skipping fitness record due to error: {e} - Record: {cert}")
             continue
@@ -206,8 +206,25 @@ def process_data(all_data):
     print(processed_output)
     return processed_output
 
+def transform_train_data(data):
+    result = []
+    # assume all keys have same index set (0..24)
+    for key in data["fitness_certificates"].keys():
+        row = {
+            "train_id": int(key),  # or f"Train-{key}" if you want string IDs
+            "fitness_certificates": data["fitness_certificates"].get(key),
+            "job_cards": data["job_cards"].get(key),
+            "branding_priority": data["branding_priority"].get(key),
+            "current_mileage": data["current_mileage"].get(key),
+        }
+        result.append(row)
+    return result
+
 def send_to_ml_api(data):
     try:
+        print("\n\nim am sending this data\n\n")
+        data = transform_train_data(data)
+        print(data)
         print(requests.post(PIPELINE_CONFIG["output"]["destination_api"]["url"], json=data))
         print("Data sent to ML model API.")
         return True
